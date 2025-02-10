0d2ce9c12429b6621e65014931dedf7658aa7ecc
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2025-02-10 17:45:40 -0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,49 @@
+'use strict';
+const { Model } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The models/index file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.ENUM,
+      values: ['organizador', 'viajente'],
+      defaultValue: 'user'
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    hooks: {
+      beforeCreate: async (user) => {
+        if(user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt)
+        }
+      }
+    }
+  });
+
+  return User;
+};
