6c25a31973202da05a91bd144ffbd1ac03705726
mw-bl <bezerram533@gmail.com>
2025-03-09 16:25:24 -0300

    arrumando identação

--- seeders/20250211172851-demo-users.js
+++ seeders/20250211172851-demo-users.js
@@ -1,29 +1,29 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Users', [
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
       {
-        name: 'João Silva',
-        email: 'joao@email.com',
-        password: '123456',
-        type: 'organizador',
+        name: "João Silva",
+        email: "joao@email.com",
+        password: "123456",
+        type: "organizador",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        name: 'Maria Souza',
-        email: 'maria@email.com',
-        password: '654321',
-        type: 'viajante',
+        name: "Maria Souza",
+        email: "maria@email.com",
+        password: "654321",
+        type: "viajante",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
     ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Usuarios', null, {});
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Usuarios", null, {});
+  },
 };
