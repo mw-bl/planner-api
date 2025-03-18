c288250a8496371fc9190b5c49a4d4a2f8762615
mw-bl <bezerram533@gmail.com>
2025-03-18 13:53:24 -0300

    concertando configurações da aplicação

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 // app.js
-import express from "express";
-import authRoutes from "./routes/authRoutes.js";
+import express from 'express';
+import authRoutes from './routes/authRoutes.js';
+import viagemRoutes from './routes/viagemRoutes.js';
+import destinoRoutes from './routes/destinoRoutes.js';
+import atividadeRoutes from './routes/atividadeRoutes.js';
+import linkRoutes from './routes/linkRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
+// Configurar CORS
+app.use(cors({
+    origin: "http://localhost:3001", // Permite apenas essa origem
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true // Se estiver usando autenticação com cookies
+}));
+
 app.use(express.json());
-app.use("/api/auth", authRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/', viagemRoutes);
+app.use('/api/', destinoRoutes);
+app.use('/api/', atividadeRoutes);
+app.use('/api/', linkRoutes);
 
 export default app;
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,14 +1,20 @@
-import jwt from "jsonwebtoken";
+//middleware/authMiddleware.js
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
-export const authenticationToken = (req, res, next) => {
-  const token = req.header("Authorization");
+dotenv.config();
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
   if (!token) {
-    return res.status(401).json({ message: "Token não fornecido!" });
+    return res.status(401).json({ message: 'Token não fornecido' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.status(403).json({ message: "Token inválido ou expirado" });
+      return res.status(403).json({ message: 'Token inválido ou expirado' });
     }
 
     req.user = user;
@@ -16,12 +22,11 @@ export const authenticationToken = (req, res, next) => {
   });
 };
 
-export const authorizedRole = (role) => {
+export const authorizeRole = (role) => {
   return (req, res, next) => {
     if (req.user.role !== role) {
-      return res.status(403).json({ message: "Acesso negado!" });
+      return res.status(403).json({ message: 'Acesso negado' });
     }
-
     next();
   };
 };
