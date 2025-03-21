0610b8b0f8ac9e56e29840a5a539fca281e73640
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2025-03-21 20:31:10 -0300

    Rota Viagem

---
+++ routes/viagemRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  createViagem,
+  getAllViagens,
+  getViagemById,
+  updateViagem,
+  deleteViagem
+} from "../controllers/viagemController.js";
+import { authenticateToken, authorizeRole } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+// router.post("/viagem", authenticateToken, authorizeRole('organizador'), createViagem);
+// router.get("/viagens", authenticateToken, getAllViagens);
+// router.get("/viagem/:id", authenticateToken, getViagemById);
+// router.put("/viagem/:id", authenticateToken, authorizeRole('organizador'), updateViagem);
+// router.delete("/viagem/:id", authenticateToken, authorizeRole('organizador'), deleteViagem);
+
+router.post("/viagem", createViagem);
+router.get("/viagens", getAllViagens);
+router.get("/viagem/:id", getViagemById);
+router.put("/viagem/:id", updateViagem);
+router.delete("/viagem/:id", deleteViagem);
+
+export default router;
