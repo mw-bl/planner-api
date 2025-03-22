b9dbe385dc937654539ec55b52764a66f9b1301e
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2025-03-22 15:00:59 -0300

    Update viagemRoutes.js

--- routes/viagemRoutes.js
+++ routes/viagemRoutes.js
@@ -6,20 +6,6 @@ import {
   updateViagem,
   deleteViagem
 } from "../controllers/viagemController.js";
-import { authenticateToken, authorizeRole } from '../middlewares/authMiddleware.js';
+import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
-
-// router.post("/viagem", authenticateToken, authorizeRole('organizador'), createViagem);
-// router.get("/viagens", authenticateToken, getAllViagens);
-// router.get("/viagem/:id", authenticateToken, getViagemById);
-// router.put("/viagem/:id", authenticateToken, authorizeRole('organizador'), updateViagem);
-// router.delete("/viagem/:id", authenticateToken, authorizeRole('organizador'), deleteViagem);
-
-router.post("/viagem", createViagem);
-router.get("/viagens", getAllViagens);
-router.get("/viagem/:id", getViagemById);
-router.put("/viagem/:id", updateViagem);
-router.delete("/viagem/:id", deleteViagem);
-
-export default router;
