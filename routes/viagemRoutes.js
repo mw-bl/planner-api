687fa9de885be3c76c1a1ce1566c0e1fd2a3b7d9
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2025-03-22 15:01:08 -0300

    Update viagemRoutes.js

--- routes/viagemRoutes.js
+++ routes/viagemRoutes.js
@@ -9,3 +9,11 @@ import {
 import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
+
+router.post("/viagem", authenticateToken, createViagem);
+router.get("/viagens", authenticateToken, getAllViagens);
+router.get("/viagem/:id", authenticateToken, getViagemById);
+router.put("/viagem/:id", authenticateToken, updateViagem);
+router.delete("/viagem/:id", authenticateToken, deleteViagem);
+
+export default router;
