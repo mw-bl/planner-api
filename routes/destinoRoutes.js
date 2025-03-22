6a7ec6ef642f8cbfc3f8db689e3c5f816fea0846
Werbetonn <werbeton.morais09@aluno.ifce.edu.br>
2025-03-22 13:33:49 -0300

    coreeção destinoRoutes

--- routes/destinoRoutes.js
+++ routes/destinoRoutes.js
@@ -9,3 +9,17 @@ import {
 import { authenticateToken, authorizeRole } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
+
+// router.post("/destino", authenticateToken, authorizeRole('organizador'), createDestino);
+// router.get("/destinos", authenticateToken, getAllDestinos);
+// router.get("/destino/:id", authenticateToken, getDestinoById);
+// router.put("/destino/:id", authenticateToken, authorizeRole('organizador'), updateDestino);
+// router.delete("/destino/:id", authenticateToken, authorizeRole('organizador'), deleteDestino);
+
+router.post("/destino", createDestino);
+router.get("/destinos", getAllDestinos);
+router.get("/destino/:id", getDestinoById);
+router.put("/destino/:id", updateDestino);
+router.delete("/destino/:id", deleteDestino);
+
+export default router;
