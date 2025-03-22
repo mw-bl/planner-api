f6f6235688c78965c1359f30ec423bbfca8fdd9e
naielyvieira <128556380+naielyvieira@users.noreply.github.com>
2025-03-22 16:35:46 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import { signUp, login, listUsers } from '../controllers/authController.js';
-import { authenticateToken, authorizeRole } from '../middlewares/authMiddleware.js';
+import { signUp, login } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/login', login);
-
-router.get('/users', authenticateToken, authorizeRole('organizador'), listUsers);
 
 export default router;
