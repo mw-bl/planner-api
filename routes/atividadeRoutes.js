8e802284ffebf8cdeb871899cafdabf0a1288172
Vicencya Nayara Diniz <115300537+Vicencya-Nayara@users.noreply.github.com>
2025-03-22 15:42:49 -0300

    Update atividadeRoutes.js

--- routes/atividadeRoutes.js
+++ routes/atividadeRoutes.js
@@ -1,5 +1,3 @@
-AtividadeRoutes
-
 import express from "express";
 import {
   createAtividade,
@@ -8,20 +6,15 @@ import {
   updateAtividade,
   deleteAtividade
 } from "../controllers/atividadeController.js";
-import { authenticateToken, authorizeRole } from '../middlewares/authMiddleware.js';
+import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-// router.post("/atividade", authenticateToken, authorizeRole('organizador'), createAtividade);
-// router.get("/atividades", authenticateToken, getAllAtividades);
-// router.get("/atividade/:id", authenticateToken, getAtividadeById);
-// router.put("/atividade/:id", authenticateToken, authorizeRole('organizador'), updateAtividade);
-// router.delete("/atividade/:id", authenticateToken, authorizeRole('organizador'), deleteAtividade);
+router.post("/atividade", authenticateToken, createAtividade);
+router.get("/atividades", authenticateToken, getAllAtividades);
+router.get("/atividade/:id", authenticateToken, getAtividadeById);
+router.put("/atividade/:id", authenticateToken, updateAtividade);
+router.delete("/atividade/:id", authenticateToken, deleteAtividade);
 
-router.post("/atividade", createAtividade);
-router.get("/atividades", getAllAtividades);
-router.get("/atividade/:id", getAtividadeById);
-router.put("/atividade/:id", updateAtividade);
-router.delete("/atividade/:id", deleteAtividade);
 
-export default router;
+export default router;
