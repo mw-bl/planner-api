47b2629bd4e0b8c209bfd883d1e17fe861514f4f
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2025-03-23 18:30:52 -0300

    apagando conteudo do model destino

--- models/destino.js
+++ models/destino.js
@@ -1,34 +1,3 @@
 
 
-export default (sequelize) => {
-  const Destino = sequelize.define("Destino", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    pais: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    estado: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    cidade: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    viagemId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "viagens",
-        key: "id",
-      },
-    },
-  });
 
-]
-  
-  return Destino;
-};
