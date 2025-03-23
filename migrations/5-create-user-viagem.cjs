a54bca0948686387881ba2193422f69bfbf679ba
Werbetonn <werbeton.morais09@aluno.ifce.edu.br>
2025-03-23 05:47:27 -0300

    updt user viagem

--- migrations/5-create-user-viagem.cjs
+++ migrations/5-create-user-viagem.cjs
@@ -1,1 +1,47 @@
 "use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("UserViagems", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users", // Nome da tabela de usuários
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        allowNull: false,
+      },
+      viagemId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Viagems", // Nome da tabela de viagens
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("UserViagems");
+  },
+};
