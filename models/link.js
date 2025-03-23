fb8a609fa631c0656d3d3683b878bf1bf7b88a6a
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2025-03-23 18:24:59 -0300

    apagando conteudo do model

--- models/link.js
+++ models/link.js
@@ -2,30 +2,4 @@
 
 
 
-export default (sequelize) => {
-  const Link = sequelize.define("Link", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    titulo: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    viagemId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "viagens",
-        key: "id",
-      },
-    },
-  });
 
- 
-  return Link;
-};
