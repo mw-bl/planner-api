08750362e790fd82705146604eeee117cb904db7
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2025-03-23 18:27:43 -0300

    apagando rota de links

--- routes/linkRoutes.js
+++ routes/linkRoutes.js
@@ -1,19 +1,1 @@
-import express from "express";
-import {
-  createLink,
-  getAllLinks,
-  getLinkById,
-  deleteLink
-} from "../controllers/linkController.js";
 
-
-const router = express.Router();
-
-router.post("/link", authenticateToken, authorizeRole('organizador'), createLink);
-router.get("/links", authenticateToken, getAllLinks);
-router.get("/link/:id", authenticateToken, getLinkById);
-router.delete("/link/:id", authenticateToken, authorizeRole('organizador'), deleteLink);
-
-
-
-export default router;
