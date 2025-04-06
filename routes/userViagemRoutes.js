8717c6e14cff5a529fca851637c984b33aa453f7
Vicencya Nayara Diniz <115300537+Vicencya-Nayara@users.noreply.github.com>
2025-04-06 20:45:40 -0300

    Update userViagemRoutes.js

--- routes/userViagemRoutes.js
+++ routes/userViagemRoutes.js
@@ -7,10 +7,10 @@ import { removerParticipante } from "../controllers/userViagemController.js";
 
 const router = express.Router();
 
-// Apenas organizadores podem associar usuários a viagens
+
 router.post("/user-viagem/:viagemId", authenticateToken, authorizeRole("organizador"), associateUserToViagem);
 router.delete("/viagens/:viagemId/participantes/:userId", authenticateToken, authorizeRole("organizador"), removerParticipante);
-// Rota para buscar usuários pelo email
+
 router.get("/user-viagem/search", authenticateToken, authorizeRole("organizador"), findUserByEmail);
 router.post('/viagens/:viagemId/confirmar', authenticateToken, confirmarParticipacao);
 export default router;
